20b77230adb0632e83ef5c281b2f6a918c5120f2
taoufiq <taoufiqibariken@gmail.com>
2025-03-22 14:36:29 +0000

    going

--- philosophers.c
+++ philosophers.c
@@ -12,22 +12,43 @@
 
 #include "philosophers.h"
 
-
-
-int main(int argc, char const **argv)
+void	usage_error(void)
 {
-	int i = 1;
-	if (argc != 5)
+	write(2, "Usage:  \"philosophers number\"", 30);
+	write(2, " \"time to die\"", 15);
+	write(2, " \"time to eat\"", 15);
+	write(2, " \"time to sleep\"", 17);
+	write(2, " (op) \"number of time each philo must eat\"\n", 44);
+	exit(1);
+}
+
+int	check_positive(char *str)
+{
+	int value;
+
+	value = ft_atoi(str);
+	if (value <= 0)
 	{
-		write(2, "Usage: \"philosophers number\" \"time to die\" \"time to eat\" \"sleep time\"\n", 70);
+		write(2, "all numbers must positive", 26),
 		exit(1);
 	}
-	while (argv[i])
-	{
-		if (ft_atoi(argv[i]) < 0)
-			(write(2, "Error", 5), exit(1));
-		i++;
-	}
-	
+	return (value);
+}
+
+int main(int argc, char **argv)
+{
+	int i = 2;
+	t_philo philo;
+
+	if (argc != 5 && argc != 6)
+		usage_error();
+	philo.number_of_philos = ft_atoi(argv[1]);
+	if (philo.number_of_philos <= 0)
+		(write (2, "number of philos must be 1 or plus", 35), exit(1));
+	philo.time_to_die = check_positive(argv[2]);
+	philo.time_to_eat = check_positive(argv[3]);
+	philo.time_to_sleep = check_positive(argv[4]);
+	if (argc == 6)
+		philo.number_of_times_philos_eat = check_positive(argv[5]);
 	return (0);
 }
--- philosophers.h
+++ philosophers.h
@@ -18,7 +18,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_philo
+{
+	int	number_of_philos;
+	int	time_to_die;
+	int time_to_eat;
+	int time_to_sleep;
+	int	number_of_times_philos_eat;
+}	t_philo;
+
+
+
 int	ft_atoi(char *nb);
+
 
 
 #endif
